a7d7fb217d567d3beba1e16bee7361a552278acd
kambehmw <kanbe.hmw@gmail.com>
2019-10-28 20:09:59 +0900

    add eval of boolean and null

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -5,6 +5,12 @@ import (
 	"github.com/kambehmw/golang-monkey/object"
 )
 
+var (
+	NULL = &object.Null{}
+	TRUE = &object.Boolean{Value: true}
+	FALSE = &object.Boolean{Value: false}
+)
+
 func Eval(node ast.Node) object.Object {
 	switch node := node.(type) {
 	// Statement
@@ -16,6 +22,8 @@ func Eval(node ast.Node) object.Object {
 	// Expression
 	case *ast.IntegerLiteral:
 		return &object.Integer{Value: node.Value}
+	case *ast.Boolean:
+		return nativeBooltoBooleanObject(node.Value)
 	}
 
 	return nil
@@ -29,4 +37,11 @@ func evalStatements(stmts []ast.Statement) object.Object {
 	}
 
 	return result
+}
+
+func nativeBooltoBooleanObject(input bool) *object.Boolean {
+	if input {
+		return TRUE
+	}
+	return FALSE
 }
